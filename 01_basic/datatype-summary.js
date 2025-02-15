65ccc4ab6977050d1eba4a91a6aa301d47b29fc7
ritikasingh9988 <ritikasingrathour99@gmail.com>
2025-02-15 17:50:53 +0000

    datatype summary

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,15 @@
+// primitive (call by value)
+// 7 types : String, Number, Boolean, null, undefined,Symbol, BigInt
+// reference type (non primitive type)
+// array, objects, function
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id===anotherid)
+const heros = {"shaktiman", "naagraj"}
+ley myobj = {
+    name : "ritika"
+    age : 21
+}
+const my function = function(){
+    console.log("hello world")
+}
