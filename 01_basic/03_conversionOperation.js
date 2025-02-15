d62726927d42dbba38a2a21c4abfd3768391b830
ritikasingh9988 <ritikasingrathour99@gmail.com>
2025-02-15 16:30:13 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = 33
+
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = 1
+let booleanisloggein = Boolean(isLoggedIn);
+console.log(booleanisloggein);
+//1 => true : 0 => false
+// "" => false
+// "ritika" => true
