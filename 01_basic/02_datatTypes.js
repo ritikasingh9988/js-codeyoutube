9dc4f58184d60692fae41b1546119b25f2441a1c
ritikasingh9988 <ritikasingrathour99@gmail.com>
2025-02-15 15:24:38 +0000

    config done

---
+++ 01_basic/02_datatTypes.js
@@ -0,0 +1,14 @@
+let age = 18
+let time;
+let state = null
+// number => 2pow53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+console.log(typeOf undefined);
+console.log(typeOf null);
